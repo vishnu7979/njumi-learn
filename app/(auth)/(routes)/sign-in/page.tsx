37308f35ecc7b8eb51564e367537a366ca38d61d
vishnu <vishnushaji797@gmail.com>
2024-03-30 20:28:57 +0530

    Folders setup

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+const SignInPage = () => {
+    return ( <div>
+        this is an unprotected page
+    </div> );
+}
+ 
+export default SignInPage;
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -4,10 +4,8 @@ import Image from "next/image";
 export default function Home() {
   return (
 <div>
-<p className="text-3xl font-medium text-sky-700">Hello World</p>
-<Button variant="destructive">
-  Click Me 
-</Button>
+<p className="text-3xl font-medium text-sky-700">  this is a protected page</p>
+
 </div>
   );
 }
